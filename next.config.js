712cec99a8ea7e2ff9665fda2ef562b12a76874d
KongTrade <vbkmmori@gmail.com>
2025-12-27 00:29:29 +0200

    Fix: Remove outputFileTracingRoot from next.config.js to fix Netlify build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
+  output: process.env.NEXT_OUTPUT_MODE || undefined,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
     optimizeCss: true,
   },
   eslint: {
