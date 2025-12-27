0f0146c6df8b71a4adad75de2345aa80385038d7
KongTrade <vbkmmori@gmail.com>
2025-12-27 05:24:35 +0200

    fix: Update Netlify config - plugin must be disabled in UI

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE || 'standalone',
+  output: process.env.NEXT_OUTPUT_MODE || undefined,
   experimental: {
     optimizeCss: true,
   },
